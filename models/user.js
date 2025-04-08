a835bffd5ffd6a63745e8f0d08c2df5866e1d3c1
Vince <vincentsilva900@gmail.com>
2025-04-07 23:50:11 -0700

    cute update on Ivy

--- models/user.js
+++ models/user.js
@@ -5,9 +5,9 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  profilePic: { type: String, default: '/default-profile.jpg' },
-  location: { type: String, default: '' },
-  bio: { type: String, default: '' },
+  profilePicInput: { type: String, default: '/default-profile.jpg' },
+  location: { type: String, },
+  bio: { type: String },
   friends: { type: [String], default: [] },
   videos: { type: [String], default: [] }
 }, { timestamps: true });
